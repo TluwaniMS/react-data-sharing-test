4bfced47aa91d910fa1c3480ab66d124ac5777eb
tluwaniMS <tluwani2020@gmail.com>
2022-03-01 10:54:41 +0200

    adding-super-power-trigger-display

--- src/displays/DetailedSuperHeroesDisplay/DetailedSuperHeroesDisplay.js
+++ src/displays/DetailedSuperHeroesDisplay/DetailedSuperHeroesDisplay.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import SuperPowerTriggerDisplay from "../SuperPowerTriggerDisplay/SuperPowerTriggerDisplay";
 
 const DetailedSuperHeroesDisplay = () => {
   const { state: hero } = useLocation();
 
-  return <div>Detailed display works.!!</div>;
+  return (
+    <div>
+      <div>{hero.name}</div>
+      <div>
+        <SuperPowerTriggerDisplay />
+      </div>
+    </div>
+  );
 };
 
 export default DetailedSuperHeroesDisplay;
---
+++ src/displays/SuperPowerTriggerDisplay/SuperPowerTriggerDisplay.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SuperPowerTriggerDisplay.css";
+
+const SuperPowerTriggerDisplay = () => {
+  return <button className="button super-power-button">Power</button>;
+};
+
+export default SuperPowerTriggerDisplay;
