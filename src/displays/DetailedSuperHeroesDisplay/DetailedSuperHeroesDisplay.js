8d797f76a2aa4ab74aa31339cf58306608d6157a
tluwaniMS <tluwani2020@gmail.com>
2022-03-01 09:37:38 +0200

    adding-method-to-retriev-data-from-route

--- src/displays/DetailedSuperHeroesDisplay/DetailedSuperHeroesDisplay.js
+++ src/displays/DetailedSuperHeroesDisplay/DetailedSuperHeroesDisplay.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const DetailedSuperHeroesDisplay = () => {
+  const { state: hero } = useLocation();
+
   return <div>Detailed display works.!!</div>;
 };
 
--- src/displays/SuperHeroesDisplay/SuperHeroesDisplay.js
+++ src/displays/SuperHeroesDisplay/SuperHeroesDisplay.js
@@ -2,7 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const SuperHeroesDisplay = ({ hero }) => {
-  return <Link to={'/detailed-super-hero-view'} className="">{hero.name}</Link>;
+  return (
+    <Link to={"/detailed-super-hero-view"} state={hero} className="">
+      {hero.name}
+    </Link>
+  );
 };
 
 export default SuperHeroesDisplay;
