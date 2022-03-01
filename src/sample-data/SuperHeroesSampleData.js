4cb5abc5f078eb8e57163746e9bd21c56f5989a0
tluwaniMS <tluwani2020@gmail.com>
2022-03-01 08:04:47 +0200

    adding-super-heroes-sample-data

--- src/sample-data/SuperHeroesSampleData.js
+++ src/sample-data/SuperHeroesSampleData.js
@@ -1,1 +1,5 @@
-export const SuperHeroes = []
+export const SuperHeroes = [
+  { name: "Elephant Man", power: "Stomp Spin" },
+  { name: "Imhotep", power: "The Pyramid Punch" },
+  { name: "Shaka Khan", power: "The Iklwa Injectors" }
+];
